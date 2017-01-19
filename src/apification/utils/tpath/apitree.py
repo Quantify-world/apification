85f7c7922b5b51397e0c9e4dc1ee9e6e26163c17
Nik <nnonexistent@gmail.com>
2017-01-19 18:44:52 +0300

    Initial implementation of @ lexem for tpath

--- src/apification/utils/tpath/apitree.py
+++ src/apification/utils/tpath/apitree.py
@@ -1,4 +1,4 @@
-from apification.utils.tpath.base import TPathError, TPathParser
+from apification.utils.tpath.base import TPathError, TPathParser, BaseLexem
 from apification.utils.tpath.proxy import ProxyNode, VirtualRoot
 
 
@@ -26,3 +26,21 @@ class ApiTreeProxy(ProxyNode):
 
 class TPathApiTreeParser(TPathParser):
     proxy = ApiTreeProxy
+
+
+@TPathApiTreeParser.lexem
+class SerializerName(BaseLexem):
+    pattern = r'@[a-zA-Z_][a-zA-Z_0-9.]*'
+
+    def __repr__(self):
+        return u'<serializer name(%s)>' % self.token[1:]
+
+    def resolve(self, iterator):
+        if iterator is None:
+            iterator = iter([self.node])
+
+        serializer_name = self.token[1:]
+        for node in iterator:
+            for name, attr_name in TODO.serializers:
+                if name == serializer_name:
+                    yield TODO.get(node, name)  # TODO is future registry of serializers
